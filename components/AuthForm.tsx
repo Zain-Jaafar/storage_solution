67b259c2336f385318b837c24b773fd1dee6711b
Zain Jaafar <zainjaafar120@gmail.com>
2025-01-19 07:49:32 +1300

    added components for auth form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,29 +15,111 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50)
-})
 
 type FormType = 'sign-in' | 'sign-out';
 
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName: formType === "sign-up" ? z.string().min(2).max(50) : z.string().optional(),
+  })
+}
+
 function AuthForm({ type }: { type: FormType }) {
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        username: "",
-      },
-    })
+  const [isLoading, setIsLoading] = useState(false);  
+  const [errorMessage, setErrorMessage] = useState("");
+
+
+  const formSchema = authFormSchema(type);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+    },
+  })
    
-    // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-      // Do something with the form values.
-      // ✅ This will be type-safe and validated.
-      console.log(values)
-    }
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
   return (
-    <div>{type}</div>
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">
+            { type === "sign-in" ? "Sign In" : "Sign Up" }
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your full name"
+                        className="shad-input"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
+
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="form-submit-button" disabled={isLoading}>
+            { type === "sign-in" ? "Sign In" : "Sign Up" }
+
+            { isLoading && (
+              <Image src="/assets/icons/loader.svg" alt="loader" height={24} width={24} className="ml2 animate-spin" />
+            )}  
+          </Button>
+          
+          { errorMessage && (
+            <p className="error-message">*{errorMessage}</p>
+          ) }
+          
+          <div className="body-2 flex justify-center">
+            <p>
+              { type === "sign-in" ? "Don't ave an account?" : "Already have an account?" }
+            </p>
+            <Link href={type === "sign-in" ? "/sign-up" : "/sign-in"} className="ml-1 font-medium text-brand">
+              { type === "sign-in" ? "Sign In" : "Sign Up" }
+            </Link>
+          </div>
+        </form>
+      </Form>
+    </>
   )
 }
 
